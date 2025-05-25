ad0e27fc1133514e11c2e87507d8b5f883d2c441
muktargazali <162722948+muktargazali@users.noreply.github.com>
2025-05-25 20:58:05 +0100

    Create my_module.py

---
+++ my_module.py
@@ -0,0 +1,15 @@
+# SIMPLE CALCULATOR TO CALCULATE WEIGHT AND HIEGHT
+#hieght :int = input("hieght")
+num1 = 832
+num2 = 1320
+
+num3 = 1280
+num4 = 1920
+
+hieght = int(input("enter hieght: "))
+new_hieght = (hieght * num1/num2)
+print(new_hieght)
+
+weight = int(input("enter weight: "))
+new_weight = (weight * num3/num4)
+print(new_weight)
